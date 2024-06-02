19f59017ac9b9c4c9c8eb5080cb4b39118768f76
unknown <lohanarodrigues52@gmail.com>
2024-06-01 23:48:55 -0300

    Meu projeto final do módulo lógica de programação do Ciclo Formativo da PretaLab, em Front-End

--- src/script.js
+++ src/script.js
@@ -1,27 +1,66 @@
-let numeroAleatorio = 0;
+let numeroAleatorio = Math.floor(Math.random() *100) + 1;
 let tentativas = 0;
+let pontuacao = 100;
+let palpitesAnteriores = [];
 
 function jogoDeAdivinhacao() {
-    /*
-    Guiado
-    ---
-    1. Não aceita palpite vazio
-    2. Dar dicas a cada palpite (maior ou menor)
-    3. Atualizar a pontuação a cada palpite errado
-    4. Mostrar todos os palpites errados
-    5. Deve-se poder reiniciar o jogo a qualquer momento
-    6. Se a pontuação chegar a zero, deve ser exibido um alerta e reiniciado o jogo
+    const palpiteDigitado = pegarPalpiteDigitado();
+
+    if (!palpiteDigitado) {
+        alert("Digite um valor válido!")
+        return;
+    }
+     if (palpiteDigitado < 1 || palpiteDigitado > 100) {
+         alert("Hello, por favor digite um número entre 1 e 100!");
+         return;
+     }
+
+     if (palpitesAnteriores.includes(palpiteDigitado)) {
+         alert("Hello, você já tentou este palpite,por favor tente outro número!")
+         return;
+     }
+
+     palpitesAnteriores = palpitesAnteriores.concat(palpiteDigitado);
+
+
+    if (palpiteDigitado === numeroAleatorio) {
+        alert("Hello, parabéns que jogada incrível você adivinhou!")
+        reiniciarJogo();
+        return;
+    } else if(palpiteDigitado > numeroAleatorio) {
+        tentativas++;
+        atualizarFeedback("Hello, o número digitado é muito alto.Tente novamente!");
+    } else if (palpiteDigitado < numeroAleatorio)  {
+        tentativas++;
+        atualizarFeedback(" Hello, o número ditado é muito baixo. Tente novamente!");
+    }
     
-    Individual
-    ---
-    7. Só deve aceitar numeros entre 1 e 100
-    8. Não deve aceitar palpite repetido
-    */
+    const novaPontuacao = 100 - (tentativas * 10);
+    atualizarPontuacao(novaPontuacao);
+    
+    const palpitesFalhos = pegarPalpitesFalhos();
+    const novosPalpitesFalhos = palpitesFalhos + " " + palpiteDigitado;
+    atualizarPalpitesFalhos(novosPalpitesFalhos);
+
+    const pontuacaoAtual = pegarPontuacao();
+    if(pontuacaoAtual === "Hello, você tem 0 pontos") {
+        alert("Perdeu! Você chegou a zero pontos.Tente novamente, boa sorte!");
+        reiniciarJogo();
+    }
+   
 
 
-    // to-do
 }
 
 function reiniciarJogo() {
-    // to-do
+    const vaiReiniciar = confirm("Deseja jogar novamente?");
+
+    if(vaiReiniciar === true) {
+        atualizarPalpitesFalhos("");
+        atualizarPontuacao(100);
+        atualizarFeedback("");
+        limparPalpiteDigitado();
+
+    }
+
 }
